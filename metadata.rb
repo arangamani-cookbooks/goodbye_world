5f25bcad1427b5aee88002366849181c3777832f
Kannan Manickam <kannan@rightscale.com>
2013-10-30 13:49:37 -0700

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'goodbye_world'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Kannan Manickam'
+maintainer_email 'me@arangamani.net'
 license          'All rights reserved'
 description      'Installs/Configures goodbye_world'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
