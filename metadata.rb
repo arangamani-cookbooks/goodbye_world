438c8d93f129f9706c9cc0ba9ac98cbeb51556a2
Kannan Manickam <kannan@rightscale.com>
2013-10-30 13:38:26 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'goodbye_world'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures goodbye_world'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
