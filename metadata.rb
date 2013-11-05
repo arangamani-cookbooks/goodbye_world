b0bd604bb34b7a3066d4415cd636ee9e922d78bd
Kannan Manickam <kannan@rightscale.com>
2013-11-05 15:55:24 -0800

    Version bump to v0.1.10

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'me@arangamani.net'
 license          'All rights reserved'
 description      'Installs/Configures goodbye_world'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.9'
+version          '0.1.10'
 
